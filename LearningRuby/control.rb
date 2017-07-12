fa4577807649d9a36e9386b87b2dcbcf71580242
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-07-11 20:36:35 -0400

    control statements

---
+++ LearningRuby/control.rb
@@ -0,0 +1,31 @@
+if count > 10
+puts "Try again"
+elsif count == 3
+    puts "You lose"
+else
+    puts "Enter a number"
+end
+
+while weight < 100 and num_pallets <= 30
+    pallet = neex_pallet()
+    weight += pallet.weight
+    num_pallets += 1
+end
+
+while line = gets
+    puts line.downcase
+end
+
+if radiation > 3000
+    puts "Danger, Will Robinson"
+end
+
+puts "Danger, Will Robinson" if radiation > 3000
+
+square = 2
+while square < 1000
+    square = square * square
+end
+
+square = 2
+square = square * square while < 1000
